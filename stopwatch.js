e89d3a65c29c07da17608016db5f200dc3d11998
Albert Stefanov <albert@ubii.me>
2025-10-08 09:14:43 +0000

    Make the timer actually tick correctly
    Use a monotonic clock to know how much time has actually passed
    (Not all clocks are created equal...)

--- stopwatch.js
+++ stopwatch.js
@@ -1,15 +1,17 @@
 'use strict';
 
 import events from 'events';
+import { hrtime } from 'node:process';
 
-const TIMESCALE = 1000;
+const TIMESCALE = 1000n * 1000n * 1000n; // ns in s
+const PRECISION = 1000; // run every 1000ms
 
 function formatTime(time) {
   time /= TIMESCALE;
 
-  const h = Math.floor(time / 60 / 60) % 24;
-  const m = Math.floor(time / 60) % 60;
-  const s = Math.floor(time) % 60;
+  const h = (time / 60n / 60n) % 24n;
+  const m = (time / 60n) % 60n;
+  const s = time % 60n;
 
   return [h, m, s].map((x) => new String(x).padStart(2, '0')).join(':');
 }
@@ -18,16 +20,18 @@ class Stopwatch extends events.EventEmitter {
   constructor() {
     super();
 
-    this.time = 0;
+    this.time = 0n;
     this.interval = undefined;
 
-    this.tickRate = TIMESCALE;
+    this.lastTs = process.hrtime.bigint();
   }
 
   start() {
     if (this.interval) return;
 
-    this.interval = setInterval(this.onTick.bind(this), this.tickRate);
+    this.lastTs = process.hrtime.bigint();
+
+    this.interval = setInterval(this.onTick.bind(this), PRECISION);
     this.emit('start:stopwatch');
     this.emit('tick:stopwatch', formatTime(this.time));
   }
@@ -42,17 +46,20 @@ class Stopwatch extends events.EventEmitter {
     this.emit('tick:stopwatch', formatTime(this.time));
   }
 
-  reset(time) {
-    this.time = time * 60 * TIMESCALE;
+  reset(minutes) {
+    this.time = BigInt(minutes * 60) * TIMESCALE;
     this.emit('reset:stopwatch', formatTime(this.time));
     this.emit('tick:stopwatch', formatTime(this.time));
   }
 
   onTick() {
-    this.time -= this.tickRate;
+    const now = process.hrtime.bigint();
+    const delta = now - this.lastTs;
+    this.lastTs = now;
+    this.time -= delta;
 
     // we must cap this, so we don't get -1 as time
-    if (this.time <= 0) this.time = 0;
+    if (this.time <= 0) this.time = 0n;
 
     if (this.time == 0) this.stop();
     else this.emit('tick:stopwatch', formatTime(this.time));
